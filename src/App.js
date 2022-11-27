1f819d1b375a44c7f51ff94cc96466d5a2105c69
Matheus <matheuscerutti96@gmail.com>
2022-11-27 19:19:32 -0300

    Layout pronto

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import Letras from "./Components/Letras"
+import React from "react"
+//import Letras from "./Components/Letras"
 
 function App() {
+  const [modojogo,setJogo]= React.useState("jogoff")
+  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
   return (
     <>
       <div className="caixona">
         <img src="assets/forca0.png"></img>
         <div className="direita">
-          <div className="botao"><button>Escolher Palavra</button></div>
+          <div className="botao"><button className="btnComecar" onClick={comecarjogo}>Escolher Palavra</button></div>
           <div className="palavra">quarentena</div>
         </div>
       </div>
-      <Letras></Letras>
+      <div className="teclado">
+            <div className="caixadeletras">
+                {alfabeto.map((alf) => <Teste letraAtual={alf}></Teste>)}
+            </div>
+            <div className="chute">
+              <div className="texto">Já sei a palavra!</div>
+              <input type="text"></input>
+              <button className = "btnChute jogon" >Chutar</button>
+            </div>
+        </div>
+     
+      
 
     </>
   );
+
+  function Teste(props) {
+    
+    return (
+        <div className={`caixaLetra + ${modojogo}`}>
+            <div className="letraDentro">{props.letraAtual}</div>
+        </div>
+    )
+
+}
+
+function comecarjogo(){
+  let comecarjogo = "caixaLetra jogon"
+  setJogo(comecarjogo)
+}
+
 }
 
 export default App;
--- src/Components/Letras.js
+++ src/Components/Letras.js
@@ -1,3 +1,5 @@
+
+
 export default function Letras() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return (
@@ -12,11 +14,18 @@ export default function Letras() {
 
 
 function Teste(props) {
+    
+
+    /*function comecarjogo(){
+        let comecarjogo = "caixaLetra jogon"
+        setJogo(comecarjogo)
+    }
+    
     return (
-        <div className="caixaLetra">
+        <div className={`caixaLetra + ${modojogo}`}>
             <div className="letraDentro">{props.letraAtual}</div>
         </div>
-    )
+    )*/
 
 
 }
