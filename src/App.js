010397e3b7099c0b45e01b07382dad9bf011204b
Matheus <matheuscerutti96@gmail.com>
2022-11-27 14:59:45 -0300

    COlocado o teclado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import Letras from "./Components/Letras"
+
 function App() {
   return (
-    <></>
+    <>
+      <div className="caixona">
+        <img src="assets/forca0.png"></img>
+        <div className="direita">
+          <div className="botao"><button>Escolher Palavra</button></div>
+          <div className="palavra">quarentena</div>
+        </div>
+      </div>
+      <Letras></Letras>
+
+    </>
   );
 }
 
--- src/Components/Letras.js
+++ src/Components/Letras.js
@@ -1,1 +1,22 @@
-const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+export default function Letras() {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    return (
+        <div className="teclado">
+            <div className="caixadeletras">
+                {alfabeto.map((alf) => <Teste letraAtual={alf}></Teste>)}
+            </div>
+            <div className="chute"></div>
+        </div>
+    )
+}
+
+
+function Teste(props) {
+    return (
+        <div className="caixaLetra">
+            <div className="letraDentro">{props.letraAtual}</div>
+        </div>
+    )
+
+
+}
